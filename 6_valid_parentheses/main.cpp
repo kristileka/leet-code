c09d55087c6181fd04c79d838d35c7227d2c5a7a
Kristi Leka <m.kristileka@gmail.com>
2023-11-26 00:07:01 +0100

    fix

--- 6_valid_parentheses/main.cpp
+++ 6_valid_parentheses/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <stack>
 
 using namespace std;
 bool isValid(string s){
-  return true;
-
-}
-int main(){
-  return 0;
+  stack<char> stack;
+  if(s.length() ==0) return true;
+  if(s.length() == 1) return false;
+  for(int i = 0 ; i<s.length();i++){
+    char c = s[i];
+    cout<<c;
+    if(c=='}' || c == ')' || c == ']'){
+      if(stack.size() == 0) return false;
+      if(c=='}' && stack.top() != '{') return false;
+      if(c == ')' && stack.top() != '(') return false;
+      if(c == ']' && stack.top() != '[') return false;
+      stack.pop();
+    }
+    else{
+      stack.push(c);
+    }
+  }
+  cout<<"end";
+  if(stack.size() == 0) return true;
+  return false;
 }
